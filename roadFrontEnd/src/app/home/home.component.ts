4571721bf2fd31807940dce5e3c5a985c8fbcd0d
Julia Evans <julevans93@users.noreply.github.com>
2020-01-20 22:53:49 -0500

    formatted map position

--- roadFrontEnd/src/app/home/home.component.ts
+++ roadFrontEnd/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapsService } from '../services/maps.service';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,22 @@ export class HomeComponent implements OnInit {
 
   services: string[];
   vehicleColor: string[];
+  lat: string = '';
+  lng: string = '';
 
-  constructor() { }
+  location: Object;
+
+  constructor(private map: MapsService) { }
 
   ngOnInit() {
     this.services = [ 'Fuel Delivery', 'Flat Tire', 'Jump Start', 'Tow', 'Locksmith' ];
     this.vehicleColor = [ 'black', 'white', 'silver', 'gold', 'red', 'green', 'blue', 'other' ];
+
+    this.map.getLocation().subscribe(data => {
+      console.log(data);
+      this.lat = data.latitude;
+      this.lng = data.longitude;
+    })
   }
 
 }
